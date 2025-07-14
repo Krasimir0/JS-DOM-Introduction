c5b64537a08e9f8a772cd918033d214f09832f4f
Krasimir Petranov <83006302+Krasimir0@users.noreply.github.com>
2025-07-14 22:41:22 +0100

    Finish exercise 3

--- 3.Accordion/accordion.js
+++ 3.Accordion/accordion.js
@@ -1,3 +1,13 @@
 function toggle() {
-  
+    const button = document.getElementsByClassName("button")[0];
+    const divExtra = document.getElementById("extra");
+    
+    if (divExtra.style.display === "none") {
+        divExtra.style.display = "block";
+        button.textContent = "Less"
+    } else {
+        divExtra.style.display = "none";
+        button.textContent = "More"
+    }
+    
 }
