d51e8143d5584f4572eef504189bac990be8fed4
Krasimir Petranov <83006302+Krasimir0@users.noreply.github.com>
2025-07-14 18:22:40 +0100

    Finish exercise 1

--- 1.Substraction/subtraction.js
+++ 1.Substraction/subtraction.js
@@ -1,3 +1,7 @@
 function subtract() {
-    //TODO
+    let firstNumber = document.getElementById("firstNumber").value;
+    let secondNumber = document.getElementById("secondNumber").value;
+    let result = document.getElementById("result");
+    let subResult = Number(firstNumber) - Number(secondNumber);
+    result.textContent = subResult;
 }
