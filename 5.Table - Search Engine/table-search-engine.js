e54573a9c0e625d20a32953daf535275c84a6ff7
Krasimir Petranov <83006302+Krasimir0@users.noreply.github.com>
2025-07-15 14:25:16 +0100

    Finish exercise 5

--- 5.Table - Search Engine/table-search-engine.js
+++ 5.Table - Search Engine/table-search-engine.js
@@ -1,3 +1,19 @@
 function solve() {
-   //TODO
+   const tdUsers = document.getElementsByTagName('td');
+   const searchField = document.getElementById('searchField').value.toLowerCase();  
+
+   for (const user of tdUsers) {
+      let parentUserTr = user.parentElement;
+      if (parentUserTr.style.backgroundColor === "yellow") {
+         parentUserTr.style.backgroundColor = '';
+      }
+   }
+
+   for (const user of tdUsers) {
+      let parentUserTr = user.parentElement;
+      let userDetail = user.textContent.toLowerCase();
+      if (userDetail.includes(searchField)) {
+         parentUserTr.style.backgroundColor = "yellow";
+      }
+   }
 }
