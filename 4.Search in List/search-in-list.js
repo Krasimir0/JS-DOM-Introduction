84aa79ba25aa87ba72784ba61af5011c7b6dd530
Krasimir Petranov <83006302+Krasimir0@users.noreply.github.com>
2025-07-15 13:20:15 +0100

    Finish exercise 4

--- 4.Search in List/search-in-list.js
+++ 4.Search in List/search-in-list.js
@@ -1,3 +1,20 @@
 function solve() {
-   
+   const ulTowns = document.getElementById('towns');
+   const liTowns = ulTowns.getElementsByTagName('li');
+   const searchTown = document.getElementById('searchText').value.toLowerCase();
+   const result = document.getElementById('result');
+
+   let matches = 0;
+   for (let town of liTowns) {
+     let townName = town.textContent.toLowerCase();
+      if (townName.includes(searchTown)) {
+         town.style.fontWeight = 'bold';
+         town.style.textDecoration = 'underline';
+         matches++;
+      } else {
+         town.style.fontWeight = 'normal';
+         town.style.textDecoration = 'none';
+      }
+   }
+   result.textContent = `${matches} matches found`;
 }
